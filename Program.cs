634684b4ee16a9396877c3a8044eeeb3c5cd8062
Kristoffer Strube <kristoffer.strube@gmail.com>
2020-10-18 23:35:12 +0200

    fix for CORS policy

--- Program.cs
+++ Program.cs
@@ -17,7 +17,11 @@ namespace CitationsDAG
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            HttpClient client = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+            client.DefaultRequestHeaders.Add("Access-Control-Allow-Origin", "*");
+            client.DefaultRequestHeaders.Referrer = new Uri(builder.HostEnvironment.BaseAddress);
+
+            builder.Services.AddScoped(sp => client);
 
             await builder.Build().RunAsync();
         }
