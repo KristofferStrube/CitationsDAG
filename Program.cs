2ab6809cc2c1c05a4de05797ce4aecd40299f613
KristofferStrube <i@kristoffer-strube.dk>
2020-10-19 23:44:26 +0200

    Added GetPapersCitingPage

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.LocalStorage;
 
 namespace CitationsDAG
 {
@@ -18,6 +19,8 @@ namespace CitationsDAG
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
         }
